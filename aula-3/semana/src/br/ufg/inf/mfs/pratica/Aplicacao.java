525072b8e064984058c097c49a01ba1b7205645b
wesleywrl <wesleyramos.ieq@gmail.com>
2020-09-25 22:12:59 -0300

    Exercicio aula-3

---
+++ aula-3/semana/src/br/ufg/inf/mfs/pratica/Aplicacao.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.mfs.pratica;
+import java.time.LocalDate;
+
+public class Aplicacao {
+	public static void main(String args[]){
+		System.out.println(LocalDate.now().getDayOfWeek());
+	}
+}
